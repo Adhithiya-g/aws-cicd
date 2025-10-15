813c030513655a0490e78ca614288942e42d211e
Adhithiya-g <adhithiya650@gmail.com>
2025-10-15 21:25:40 +0530

    feat: Update welcome message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "This update was deployed automatically by my awesome CI/CD pipeline! Version 2.0"
+    return "This update was deployed automatically by my awesome CI/CD pipeline! Version 3.0"
 
 if __name__ == '__main__':
     # Run on port 80 to be accessible from the browser
